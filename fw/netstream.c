6141a7745ba341f6a35e88a2a4d4cf364d2883c7
tinito <martino.migliavacca@gmail.com>
2013-09-20 23:39:08 +0200

    clean up

--- fw/netstream.c
+++ fw/netstream.c
@@ -4,102 +4,60 @@
 #include "ch.h"
 #include "hal.h"
 
-/*
- NetStream *mystream = NULL;
-
- static void event_callback(struct netconn *conn, enum netconn_evt evt, u16_t len) {
- struct netbuf *inbuf;
- char *buf;
- u16_t buflen;
- err_t err;
-
- (void) len;
-
- if (conn && mystream) {
- chSysLock()
- if (evt == NETCONN_EVT_RCVPLUS) {
- err = netconn_recv(conn, &inbuf);
-
- if (err != ERR_OK) {
- chSysUnlock();
- return;
- }
-
- netbuf_data(inbuf, (void **) &buf, &buflen);
-
- while (buflen--) {
- if (chIQIsEmptyI(&mystream->iqueue)) {
- chnAddFlagsI(mystream, CHN_INPUT_AVAILABLE);
- }
- if (chIQPutI(&mystream->iqueue, buf++) < Q_OK) {
- chnAddFlagsI(mystream, SD_OVERRUN_ERROR);
- break;
- }
- }
- chSysUnlock();
- }
- }
- }
-
- */
-
 static size_t write(void *ip, const uint8_t *bp, size_t n) {
-	NetStream *sp = ip;
+   NetStream *sp = ip;
 
-	return netconn_write_partly(sp->conn, bp, n, NETCONN_COPY, NULL);
+   return netconn_write_partly(sp->conn, bp, n, NETCONN_COPY, NULL);
 }
 
 static size_t read(void *ip, uint8_t *bp, size_t n) {
-	NetStream *sp = ip;
-	struct netbuf *inbuf;
-	err_t err;
-	size_t w ;
+   NetStream *sp = ip;
+   err_t err;
 
-	/* Check if there is still data from last packet. */
-	if (sp->in_offset == 0) {
-		/* Wait for new packet. */
-		do {
-			err = netconn_recv(sp->conn, &inbuf);
-		} while (err != ERR_OK);
-	}
+   /* If last input buffer was completely consumed, wait for a new packet. */
+   while (sp->inbuf == NULL) {
+      /* Wait for new packet. */
+      err = netconn_recv(sp->conn, &sp->inbuf);
+      if (err != ERR_OK) {
+         /* Connection closed (or any other errors). */
+         return 0;
+      }
+   }
 
-	chSysLock();
-	w = netbuf_copy_partial(inbuf, bp, n, sp->in_offset);
-	sp->in_offset += w;
-	if (sp->in_offset >= netbuf_len(inbuf)) {
-		netbuf_free(inbuf);
-		sp->in_offset = 0;
-	}
-	chSysUnlock();
+   netbuf_copy_partial(sp->inbuf, bp, n, sp->in_offset);
+   sp->in_offset += n;
 
-	/*
-	 netbuf_data(inbuf, (void **) &buf, &buflen);
-	 while (n-- && buflen--) {
-	 *bp++ = *buf++;
-	 w++;
-	 }
-	 */
+   /* Check if there is more data to read. */
+   if (sp->in_offset >= netbuf_len(sp->inbuf)) {
+      n -= (sp->in_offset - netbuf_len(sp->inbuf));
+      netbuf_delete(sp->inbuf);
+      sp->in_offset = 0;
+      sp->inbuf = NULL;
+   }
 
-	return w;
+   return n;
 }
 
 static msg_t put(void *ip, uint8_t b) {
-	return (write(ip, &b, 1) == 1 ? Q_OK : Q_RESET);
+   return (write(ip, &b, 1) == 1 ? Q_OK : Q_RESET);
 }
 
 static msg_t get(void *ip) {
-	uint8_t b;
+   uint8_t b;
 
-	return (read(ip, &b, 1) == 1 ? b : Q_RESET);
+   return (read(ip, &b, 1) == 1 ? b : Q_RESET);
 }
 
 static const struct NetStreamVMT vmt = { write, read, put, get };
 
-uint8_t buffer[64];
+void nsObjectInit(NetStream *sp) {
 
-void nsObjectInit(NetStream *sp, struct netconn * conn) {
+   sp->vmt = &vmt;
+   sp->inbuf = NULL;
+   sp->in_offset = 0;
+}
 
-	sp->vmt = &vmt;
-	sp->conn = conn;
-	sp->in_offset = 0;
+void nsStart(NetStream *sp, struct netconn * conn) {
+
+   sp->conn = conn;
 }
--- fw/netstream.h
+++ fw/netstream.h
@@ -4,27 +4,27 @@
 #include "ch.h"
 
 #define _net_stream_data \
-  _base_sequential_stream_data \
-  struct netconn * conn; \
-  size_t in_offset;
+   _base_sequential_stream_data \
+   struct netconn * conn; \
+   struct netbuf * inbuf; \
+   size_t in_offset;
 
 struct NetStreamVMT {
-	_base_sequential_stream_methods
+   _base_sequential_stream_methods
 };
 
 /**
- * @extends BaseSequentialStream
- *
- * @brief Memory stream object.
- */
+* @extends BaseSequentialStream
+*/
 typedef struct {
-	const struct NetStreamVMT *vmt;_net_stream_data
+   const struct NetStreamVMT *vmt;_net_stream_data
 } NetStream;
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-void nsObjectInit(NetStream *sp, struct netconn * conn);
+void nsObjectInit(NetStream *sp);
+void nsStart(NetStream *sp, struct netconn * conn);
 #ifdef __cplusplus
 }
 #endif
